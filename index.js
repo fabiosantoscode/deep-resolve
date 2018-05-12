90e8dc9279c3a0a267b25da3d2cf401a69b2d54c
Fábio Santos <fabiosantosart@gmail.com>
2018-05-12 11:54:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = function deepResolve(p) {
+  return Promise.resolve(p).then(function (p) {
+    if (typeof p.length === 'number' && typeof p !== 'string') {
+      return Promise.all(p.map(deepResolve))
+    } else if (p !== null && typeof p === 'object') {
+      var keys = Object.keys(p)
+      var values = keys.map(function(key) { return deepResolve(p[key]) })
+      return Promise.all(values)
+        .then(function(values) {
+          var out = {}
+          for (var i = 0; i < keys.length; i++) {
+            out[keys[i]] = values[i]
+          }
+          return out
+        })
+    } else {
+      return Promise.resolve(p)
+    }
+  })
+}
---
+++ test.js
@@ -0,0 +1,48 @@
+'use strict'
+
+var test = require('tape')
+var deepResolve = require('.')
+
+test('resolve a number', function(t) {
+  t.plan(1)
+
+  deepResolve(1).then(function(n) {
+    t.equal(n, 1)
+  })
+})
+
+test('resolve an object', function(t) {
+  t.plan(1)
+
+  deepResolve({
+    foo: Promise.resolve({ bar: Promise.resolve(3) }),
+    baz: '123',
+    qux: [1, 2, '3', [Promise.resolve(4), {foo: Promise.resolve('bar')}]]
+  }).then(function (resolved) {
+    t.deepEqual(resolved, {
+      foo: {
+        bar: 3
+      },
+      baz: '123',
+      qux: [1, 2, '3', [4, {foo: 'bar'}]]
+    })
+  })
+})
+
+test('resolve an array', function (t) {
+  t.plan(1)
+
+  deepResolve([
+    1,
+    Promise.resolve(2),
+    [3],
+    Promise.resolve([4])
+  ]).then(function(resolved) {
+    t.deepEqual(resolved, [
+      1,
+      2,
+      [3],
+      [4]
+    ])
+  })
+})
